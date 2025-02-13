9ca0a15afa5faacaa755997f8ce7f1cca740e456
rattphumi <darkdamonig@gmail.com>
2025-02-14 00:05:53 +0700

    lab19_complete

--- lab19.h
+++ lab19.h
@@ -68,7 +68,38 @@ void Unit::newTurn(){
 /////////////////////////////////////////////////////////////////////////////////////
 //Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
 /////////////////////////////////////////////////////////////////////////////////////
+bool Unit::isDead(){
+	if(hp <= 0) return true;
+	else return false;
+}
 
+void Unit::guard(){
+	guard_on = true;
+}
+
+int Unit::beAttacked(int oppatk){
+	int a;
+	if(!guard_on){
+		a = oppatk - def;
+		hp -= a;
+	}else if(guard_on){
+		a = (oppatk - def)/3;
+		hp -= a;
+	}
+	return a;
+}
+
+int Unit::attack(Unit &A){
+	return A.beAttacked(atk);
+}
+
+int Unit::heal(){
+	int hpn = hp;
+	int heal = rand()%21+10;
+	hp += heal;
+	if(hp > hpmax) hp = hpmax;
+	return hp - hpn;
+}
 
 
 void drawScene(char p_action,int p,char m_action,int m){
